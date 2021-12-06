c79a4d23738cd7bae914c81bde5bcf6dd0ba36e3
tqer39 <tqer39@gmail.com>
2021-12-07 05:05:28 +0900

    TFCの基本構成

---
+++ aws-audit/variables.tf
@@ -0,0 +1,29 @@
+variable "Environment" {
+  description = "環境（dev | stg | prd）の種類。"
+  type        = string
+  default     = "prd"
+}
+
+variable "IaC" {
+  description = "IaC（Infrastructure as Code）で使用したソフトウェア。"
+  type        = string
+  default     = "Terraform"
+}
+
+variable "TFC_Organization" {
+  description = "Terraform Cloud の Organization の名前。"
+  type        = string
+  default     = "private-aws"
+}
+
+variable "TFC_Workspace" {
+  description = "Terraform Cloud の Workspace の名前。"
+  type        = string
+  default     = "aws-audit"
+}
+
+variable "region" {
+  description = "当プロジェクトにおけるデフォルトのリージョン。"
+  default     = "ap-northeast-1"
+  type        = string
+}
